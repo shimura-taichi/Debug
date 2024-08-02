a4b0e3f252558c8c637dc2aa1f162781608cd59f
shimura-taichi <f7c-f13l-tnmfmf@outlook.jp>
2024-08-02 22:34:25 +0900

    デバッグ課題1時間目

---
+++ デバッグ課題/scriptデバッグ.js
@@ -0,0 +1,15 @@
+function toOneDimension( previousValue, currentValue){
+  return previousValue.concat( currentValue);
+}
+//↓修正しない
+var sampleArrayA = [
+  ['A1','A2','A3'],['B1','B2','B3'],['C1','C2','C3'],
+];
+
+var sampleArrayB = sampleArrayA.reduce(toOneDimension);
+//↓修正しない
+for(var counterVar= 0; counterVar< sampleArrayB.length; counterVar++){
+  document.write('sampleArrayB['+counterVar+']:');
+  document.write(sampleArrayB[counterVar] );
+  document.write( '<br/>');
+}
