6366e5f5fd97c5455ad5111d35acc9299a7b6954
shimura-taichi <f7c-f13l-tnmfmf@outlook.jp>
2024-09-15 20:51:23 +0900

    デバッグconsoleの追加

--- デバッグ課題/scriptデバッグ.js
+++ デバッグ課題/scriptデバッグ.js
@@ -1,16 +1,27 @@
 function toOneDimension(previousValue,currentValue){
-  return currentValue.concat(previousValue);
+
+  console.log('previousValue:', previousValue);
+  console.log('currentValue:', currentValue);
+
+  const result = currentValue.concat(previousValue);
+  console.log(result);
+
+  return result;
+
 }
 //↓修正しない
 var sampleArrayA = [
-  ['A1','A2','A3'],['B1','B2','B3'],['C1','C2','C3']
+ ['A1','A2','A3'],['B1','B2','B3'],['C1','C2','C3']
 ];
-
 
 var sampleArrayB = sampleArrayA.reduce(toOneDimension);
 
 //↓修正しない
 for(var counterVar= 0; counterVar< sampleArrayB.length; counterVar++){
+  console.log('sampleArrayB['+counterVar+']:');
+  console.log(sampleArrayB[counterVar] );
+  console.log( '<br/>');
+
   document.write('sampleArrayB['+counterVar+']:');
   document.write(sampleArrayB[counterVar] );
   document.write( '<br/>');
